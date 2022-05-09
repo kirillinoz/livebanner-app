6f94a1b7bcb170b90fdf1250557299188f747e7d
Kirill Inoz <63226427+kirillinoz@users.noreply.github.com>
2022-05-09 12:58:32 +0200

    Analytics :chart_with_upwards_trend: - Initialized Google Analytics

--- plugins/firebase.js
+++ plugins/firebase.js
@@ -1,4 +1,5 @@
 import { initializeApp } from 'firebase/app'
+import { getAnalytics } from 'firebase/analytics'
 
 const firebaseConfig = {
   apiKey: process.env.firebaseApiKey,
@@ -7,6 +8,8 @@ const firebaseConfig = {
   storageBucket: 'livebanner-77c6a.appspot.com',
   messagingSenderId: '915903780304',
   appId: '1:915903780304:web:7281be701e6c60b857d5d5',
+  measurementId: 'G-0S9KGXXRC6',
 }
 
-initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig)
+getAnalytics(app)
